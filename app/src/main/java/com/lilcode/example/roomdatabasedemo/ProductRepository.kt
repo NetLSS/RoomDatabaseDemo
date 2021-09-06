e2388aef5ce6e2fc251d9e5621ebf6d96ef58c36
NetLSS <futureprogrammer@naver.com>
2021-09-06 16:55:53 +0900

    Feat: ProductRepository 정의

---
+++ app/src/main/java/com/lilcode/example/roomdatabasedemo/ProductRepository.kt
@@ -0,0 +1,57 @@
+package com.lilcode.example.roomdatabasedemo
+
+import android.app.Application
+import androidx.core.graphics.createBitmap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.*
+
+class ProductRepository(application: Application) {
+    val searchResult = MutableLiveData<List<Product>>()
+
+    private var productDao: ProductDao?
+
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    val allProducts: LiveData<List<Product>>?
+
+    init {
+        val db: ProductRoomDatabase? = ProductRoomDatabase.getDatabase(application)
+        productDao = db?.productDao()
+        allProducts = productDao?.getAllProducts()
+    }
+
+    fun insertProduct(newProduct: Product) {
+        coroutineScope.launch(Dispatchers.IO){
+            asyncInsert(newProduct)
+        }
+    }
+
+    private suspend fun asyncInsert(product: Product) {
+        productDao?.insertProduct(product)
+    }
+
+    fun deleteProduct(name: String) {
+        coroutineScope.launch(Dispatchers.IO){
+            asyncDelete(name)
+        }
+    }
+
+    private suspend fun asyncDelete(name: String) {
+        productDao?.deleteProduct(name)
+    }
+
+    fun findProduct(name: String) {
+        coroutineScope.launch(Dispatchers.Main) {
+            searchResult.value = asyncFind(name).await()
+        }
+    }
+
+    private suspend fun asyncFind(name: String): Deferred<List<Product>?> {
+        coroutineScope.async(Dispatchers.IO){
+            return@async productDao?.findProduct(name)
+        }
+    }
+
+
+}
