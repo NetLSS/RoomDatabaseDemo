bfd434f10d60353717bd5c4b8e0edbe42be2df03
NetLSS <futureprogrammer@naver.com>
2021-09-06 17:07:11 +0900

    Feat: MainViewModel.kt 정의

--- app/src/main/java/com/lilcode/example/roomdatabasedemo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/lilcode/example/roomdatabasedemo/ui/main/MainViewModel.kt
@@ -1,7 +1,40 @@
 package com.lilcode.example.roomdatabasedemo.ui.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel // application context 를 사용하기 위해 해당 클래스 사용.
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.lilcode.example.roomdatabasedemo.Product
+import com.lilcode.example.roomdatabasedemo.ProductRepository
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: ProductRepository = ProductRepository(application)
+    private val allProducts: LiveData<List<Product>>?
+    private val searchResult: MutableLiveData<List<Product>>
+
+    init {
+        allProducts = repository.allProducts
+        searchResult = repository.searchResult
+    }
+
+    fun insertProduct(product: Product) {
+        repository.insertProduct(product)
+    }
+
+    fun findProduct(name: String) {
+        repository.findProduct(name)
+    }
+
+    fun deleteProduct(name: String) {
+        repository.deleteProduct(name)
+    }
+
+    fun getSearchResults(): MutableLiveData<List<Product>> {
+        return searchResult
+    }
+
+    fun getAllProducts(): LiveData<List<Product>>? {
+        return allProducts
+    }
 }
